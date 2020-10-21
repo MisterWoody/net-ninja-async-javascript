758bd48958cb084f64832bdcc13eee39c08a0cb2
Tony Watkins <tony@llifion.net>
2020-10-21 13:36:01 +0100

    Refactor to make more readable, removing the chrsitmas tree nesting nad making it look more sequential and easier to reason about

--- async.js
+++ async.js
@@ -7,25 +7,29 @@ window.onload = function () {
     $.ajax({
         type: "GET",
         url: "data/tweets.json",
-        success: function (data) {
-            console.log(data);
-            $.ajax({
-                type: "GET",
-                url: "data/friends.json",
-                success: function (data) {
-                    console.log(data);
-                    $.ajax({
-                        type: "GET",
-                        url: "data/videos.json",
-                        success: function (data) {
-                            console.log(data);
-                        },
-                        error: handleError
-                    });
-                },
-                error: handleError
-            });
-        },
+        success: cbTweets,
         error: handleError
     });
-};
+
+    function cbTweets(data) {
+        console.log(data);
+        $.ajax({
+            type: "GET",
+            url: "data/friends.json",
+            success: cbFriends,
+            error: handleError
+        });
+    }
+
+    function cbFriends(data) {
+        console.log(data);
+        $.ajax({
+            type: "GET",
+            url: "data/videos.json",
+            success: function (data) {
+                console.log(data);
+            },
+            error: handleError
+        });
+    }
+}
