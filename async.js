d7bcc85089801b4c1d21952d233a020ad1c7b645
Tony Watkins <tony@llifion.net>
2020-10-25 09:44:31 +0000

    Create a generator to async retrieve json data files on demand while there is a next.NB Generators are syntactic sugar over iterators

--- async.js
+++ async.js
@@ -1,12 +1,30 @@
 window.onload = function () {
 
-    function* gen() {
-        var x = yield 10;
-        console.log(x);
-    }
+    genWrap(function* () {
 
-    var myGen = gen();
-    console.log(myGen.next());
-    console.log(myGen.next(6));
+        var tweets = yield $.get("data/tweets.json");
+        console.log(tweets);
+
+        var friends = yield $.get("data/friends.json");
+        console.log(friends);
+
+        var videos = yield $.get("data/videos.json");
+        console.log(videos);
+    });
+
+    function genWrap(generator) {
+
+        var gen = generator();
+
+        function handle(yielded) {
+            if (!yielded.done) {
+                yielded.value.then(function (data) {
+                    return handle(gen.next(data));
+                })
+            }
+        }
+
+        return handle(gen.next());
+    }
 
 };
